ebdaf74d6bf6b9cc45803325bb06c2c76c122a40
oereo <dlstpgns0406@gmail.com>
2021-07-12 00:30:33 +0900

    style: fix text align

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = 5000
 const dbConfig = require('./secrets.json')
 
 const mongoose = require('mongoose')
+
 mongoose.connect(`mongodb+srv://oereo:${dbConfig.mongoDBPassword}@boilerplate.t4pdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
     userNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected')).catch(err => console.log(err))
-
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
