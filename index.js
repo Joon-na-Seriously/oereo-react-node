7b691adc0798c0a380d40312a421c7ee9b2eb931
oereo <dlstpgns0406@gmail.com>
2021-07-12 16:17:33 +0900

    feat: add search email in datebase and password validation check logic in index.js

--- index.js
+++ index.js
@@ -29,4 +29,22 @@ app.post('/register', (req, res) => {
     });
 })
 
+app.post('/login', (req, res) => {
+    // search request email in datebase
+    User.findOne({emial: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+    })
+    // password validation check
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if(!isMatch) return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+    })
+    // if password valid, generate token
+})
+
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
