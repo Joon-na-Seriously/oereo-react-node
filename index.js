127d8393b86c7188bb803633893f8e722c1f3f07
oereo <dlstpgns0406@gmail.com>
2021-07-15 00:19:31 +0900

    feat: add user auth api

--- index.js
+++ index.js
@@ -60,7 +60,17 @@ app.post('/api/users/login', (req, res) => {
 })
 
 app.get('/api/users/auth', auth, (req, res)=> {
-
+    // middleware -> true
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
 })
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
