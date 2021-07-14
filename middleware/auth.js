23b6e46f7e65a113d3ef4fb933cb2f1a462eca36
oereo <dlstpgns0406@gmail.com>
2021-07-14 20:00:11 +0900

    feat: add auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+
+
+let auth = (req, res, next) => {
+    // Authentication
+    // get token from client cookie
+    // find user from token
+    // if user is exist, authenticated
+    // if user is not exist, not authenticated
+}
+
+
+module.exports = {auth};
