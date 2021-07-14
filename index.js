30e874e325b21025374939bdcb6ceecd898bec9c
oereo <dlstpgns0406@gmail.com>
2021-07-15 01:19:07 +0900

    feat: add logout api

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const { auth } = require('./middleware/auth');
 
 const config = require('./config/key');
 
-app.use(bodyParser.urlencoded({extended: true}));  // application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));  // application/x-www-form-urlencoded
 app.use(bodyParser.json());  // application/json
 app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
-    userNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+    userNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected')).catch(err => console.log(err))
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world!!!')
+    res.send('hello world!!!')
 })
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
     user.save((err, userInfo) => {
-        if (err) return res.json({success:false, err})
+        if (err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
         })
@@ -34,32 +34,32 @@ app.post('/api/users/register', (req, res) => {
 
 app.post('/api/users/login', (req, res) => {
     // search request email in datebase
-    User.findOne({emial: req.body.email}, (err, user) => {
-        if(!user) {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
             return res.json({
                 loginSuccess: false,
                 message: "제공된 이메일에 해당하는 유저가 없습니다."
             })
         }
-    })
-
-    // password validation check
-    user.comparePassword(req.body.password, (err, isMatch) => {
-        if(!isMatch) return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."});
         
-        // if password valid, generate token
-        user.generateToken((err, user) => {
-            if (err) return res.status(400).send(err);
+        // password validation check
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
 
-            //save token in cookie
-            res.cookie("x_auth", user.token)
-            .status(200)
-            .json({loginSuccess: true, userId: user._id});
+            // if password valid, generate token
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                //save token in cookie
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+            })
         })
     })
 })
 
-app.get('/api/users/auth', auth, (req, res)=> {
+app.get('/api/users/auth', auth, (req, res) => {
     // middleware -> true
     res.status(200).json({
         _id: req.user._id,
@@ -73,4 +73,15 @@ app.get('/api/users/auth', auth, (req, res)=> {
     })
 })
 
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" },
+        (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
