6cc9c0764b240869e6650e663aeae4c716e750e2
oereo <dlstpgns0406@gmail.com>
2021-07-15 00:18:22 +0900

    feat: add auth in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-
+const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
     // Authentication
@@ -6,6 +6,17 @@ let auth = (req, res, next) => {
     // find user from token
     // if user is exist, authenticated
     // if user is not exist, not authenticated
+
+    let token = req.cookies.x_auth;
+    
+    User.findByToken(token, (err, user) => {
+        if(err) throw err;
+        if(!user) return res.json({ isAuth: false, error: true});
+
+        req.token = token;
+        req.user = user;
+        next();  // middleware 이후 계속 갈 수 있도록
+    });
 }
 
 
