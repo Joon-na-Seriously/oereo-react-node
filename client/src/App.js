33effbaa721ae3a494e8ab40b645f9535805f143
oereo <dlstpgns0406@gmail.com>
2021-07-15 23:54:32 +0900

    style: fix .prettier format and fix error

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,31 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        {/*
+   return (
+      <Router>
+         <div>
+            {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
           matches the current URL. Use a <Switch> any time
           you have multiple routes, but you want only one
           of them to render at a time
         */}
-        <Switch>
-          <Route exact path="/" component={LandingPage} />  
-          <Route exact path="/login" component={LoginPage} />
-          <Route path="/register">
-            <RegisterPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+            <Switch>
+               <Route exact path="/" component={LandingPage} />
+               <Route exact path="/login" component={LoginPage} />
+               <Route path="/register">
+                  <RegisterPage />
+               </Route>
+            </Switch>
+         </div>
+      </Router>
+   )
 }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 function RegisterPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+   return <div></div>
 }
 
 export default RegisterPage
